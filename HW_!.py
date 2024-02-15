4b1b1b025ac84d99ea76686af8fa6253956e72ac
Carlechinno <carmelwilsonds@gmail.com>
2024-02-15 18:20:50 +0200

    Added function 4 solution - recursive function to print all the numbers between two given parameters

--- HW_!.py
+++ HW_!.py
@@ -10,7 +10,7 @@ def is_prime(n: int):
     if n % 2 == 0 or n % 3 == 0:
         return False
 
-    i = 5
+    i = 5 # we already checked for 1 , 2 ,3 and 4 isnt a prime number so we can start from 5
     while i * i <= n:
         if (n % i == 0 or
                 n % (i + 2) == 0):
@@ -19,7 +19,7 @@ def is_prime(n: int):
 
     return True
 
-
+# Question 1 --> function to return the sum of all prime factors
 def factor_sum(x:int):
     sum = 0
     for i in range(1, x-1):
@@ -28,3 +28,12 @@ def factor_sum(x:int):
                 sum += i
 
     return sum
+
+
+# Question 4 --> A recursive function to return all the numbers between a and b excluding a 3rd number c
+def printnumbers(x:int,y:int,z:int):
+    if x == y:
+        return print(y)
+    if x < y and x!=z:
+        print(x)
+    printnumbers(x+1,y,z)
